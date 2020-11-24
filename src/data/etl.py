53c0401a4356d36715ffd3f6e253ba5888cdff90
Parker Grey Addison <pgaddiso@ucsd.edu>
2020-11-24 01:35:22 -0800

    Parallelize pipeline; Try out LSTM

--- src/data/etl.py
+++ src/data/etl.py
@@ -5,13 +5,79 @@ import glob
 import os
 import re
 
-import multiprocessing
+# import multiprocessing
+import concurrent.futures
+
+global out
+
+def process_file(file):
+    global out
+    outdir = out
+
+    print(f"Processing {file}")
+    fname = os.path.basename(file)
+    fnlower = fname.lower()
+
+    # Load in the raw data
+    df = pd.read_csv(file)
+
+    # Cleaning and preprocessing (reformatting) of the data are file- and
+    # config-agnostic.
+    cleaned = src.data.clean(df)
+    preprocessed = src.data.preprocess(cleaned)
+    df = preprocessed
+
+    # # Calculate additional columns for features to be engineered on.
+    # # Currently only inter arrival time is possible.
+    # column_map = {
+    #     "inter_arrival_time": src.features.extending.inter_arrival_time
+    # }
+    # # Yeah yeah, this is related to features... whether or not it belongs in
+    # # the preprocessing step is questionable. I haven't even decided myself!
+    # extensions = [column_map[column] for column in add_columns]
+    # extended = src.features.extend(preprocessed, *extensions)
+    # df = extended
+
+    # Extract labels from the file name and save as either a streaming or
+    # browsing file.
+    #
+    # Support for metadata (like streaming provider, resolution, etc) is
+    # still pending. Thinking about hdf5 format but not sure...
+    streaming_providers = [
+        "youtube", "hbomax", "disneyplus", "canvas", "amazonprime", "hulu",
+        "vimeo", "netflix", "espnplus",
+    ]
+    browsing_words = [
+        "novideo", "nostream", "general", "browsing", 
+    ]
+    is_streaming = re.search('|'.join(streaming_providers), fnlower)
+    is_browsing = re.search('|'.join(browsing_words), fnlower)
+    if not (is_streaming or is_browsing) or (is_streaming and is_browsing):
+        # print(f"File {fname} does not match naming conventions, cannot determine activity. Ignoring")
+        return False
+    # meta = {
+    #     'activity': 'streaming' if is_streaming else 'browsing',
+    # }
+
+    # Save preprocessed file with new format:
+    # {streaming | browswing }-<original file name>.csv
+    #
+    # Notice we don't have index=False -- it's important to keep the index
+    # since it is set to the packet arrival time.
+    if is_streaming:
+        df.to_csv(os.path.join(outdir, f"streaming-{fname}"))
+    else:
+        df.to_csv(os.path.join(outdir, f"browsing-{fname}"))
+
+    return True
 
 def etl(source, outdir, pattern, add_columns):
     """
     Loads data files from source matching a glob pattern, then performs cleaning
     and preprocessing steps and saves each file to outdir.
     """
+    global out
+    out = outdir
 
     # Make sure source exists. If not, then make soft links.
     if not os.path.exists(source):
@@ -32,68 +98,29 @@ def etl(source, outdir, pattern, add_columns):
     # Otherwise, a null or empty pattern will default to preprocessing all.
     to_preprocess = glob.glob(os.path.join(source, pattern or "*"))
     # We're only interested in the vpn data
-    to_preprocess = filter(lambda file: "novpn" not in file.lower(), to_preprocess)
+    to_preprocess = list(filter(lambda file: "novpn" not in file.lower(), to_preprocess))
 
     #! TODO: Parallelize
     #
     #  I attempted to do so, but ran into a NameError about half way through
     #  the data! (strange).
-    for file in to_preprocess:
+    
 
-        print(f"Processing {file}")
-        fname = os.path.basename(file)
-        fnlower = fname.lower()
+    # values = [1,2,3,4]
+    
+    # with concurrent.futures.ProcessPoolExecutor(4) as executor:
+    #     results = executor.map(square, values)
+    #     print(list(results))
 
-        # Load in the raw data
-        df = pd.read_csv(file)
+    with concurrent.futures.ProcessPoolExecutor(5) as executor:
+        results = executor.map(process_file, to_preprocess)
+    
+        print(list(results))
+        # for result in results:
+        #     print(result)
 
-        # Cleaning and preprocessing (reformatting) of the data are file- and
-        # config-agnostic.
-        cleaned = src.data.clean(df)
-        preprocessed = src.data.preprocess(cleaned)
-        df = preprocessed
-
-        # Calculate additional columns for features to be engineered on.
-        # Currently only inter arrival time is possible.
-        column_map = {
-            "inter_arrival_time": src.features.extending.inter_arrival_time
-        }
-        # Yeah yeah, this is related to features... whether or not it belongs in
-        # the preprocessing step is questionable. I haven't even decided myself!
-        extensions = [column_map[column] for column in add_columns]
-        extended = src.features.extend(preprocessed, *extensions)
-        df = extended
-
-        # Extract labels from the file name and save as either a streaming or
-        # browsing file.
-        #
-        # Support for metadata (like streaming provider, resolution, etc) is
-        # still pending. Thinking about hdf5 format but not sure...
-        streaming_providers = [
-            "youtube", "hbomax", "disneyplus", "canvas", "amazonprime", "hulu",
-            "vimeo", "netflix", "espnplus",
-        ]
-        browsing_words = [
-            "novideo", "nostream", "general", "browsing", 
-        ]
-        is_streaming = re.search('|'.join(streaming_providers), fnlower)
-        is_browsing = re.search('|'.join(browsing_words), fnlower)
-        if not (is_streaming or is_browsing) or (is_streaming and is_browsing):
-            print(f"File {fname} does not match naming conventions, cannot determine activity. Ignoring")
-            return
-        # meta = {
-        #     'activity': 'streaming' if is_streaming else 'browsing',
-        # }
-
-        # Save preprocessed file with new format:
-        # {streaming | browswing }-<original file name>.csv
-        #
-        # Notice we don't have index=False -- it's important to keep the index
-        # since it is set to the packet arrival time.
-        if is_streaming:
-            df.to_csv(os.path.join(outdir, f"streaming-{fname}"))
-        else:
-            df.to_csv(os.path.join(outdir, f"browsing-{fname}"))
+    # return True
+    
     # pool = multiprocessing.Pool(4)
     # pool.map(process_file, to_preprocess)
     
--- src/features/apply.py
+++ src/features/apply.py
@@ -5,6 +5,41 @@ import glob
 import os
 
 import scipy.signal
+import numpy as np
+
+import concurrent.futures
+
+def get_signal(file, resample_length='100ms', chunk_length='90s', rolling_length='1s'):
+
+    print(f"Feature engineering {file}")
+
+    # All preprocessed files follow the same naming convention -- either
+    # streaming-<originalfilename>.csv or browsing-<original>.csv
+    fname = os.path.basename(file)
+    splitted = fname.split('-')
+    activity = splitted[0]
+    originalfilename = '-'.join(splitted[1:])
+
+    df = pd.read_csv(file, index_col='ptime')
+    df.index = pd.to_timedelta(df.index)
+    df = src.features.extend(df, src.features.extending.inter_arrival_time)
+    resampled = df.resample(resample_length).sum().reset_index()
+    slice_indices = np.argwhere(
+        resampled.ptime.isin(
+            pd.timedelta_range(resampled.ptime.min(), resampled.ptime.max(), freq=chunk_length)
+        )
+        .values
+    ).ravel()
+    chunks = np.split(resampled, slice_indices)[1:-1]
+
+    signals = []
+
+    for chunk in chunks:
+        signal = tuple(chunk.rolling(rolling_length, on='ptime').psize.sum().values)
+        signals.append((originalfilename, activity, signal))
+
+    return signals or [None, None, None]
+
 
 def apply_features(source, outdir, outfile, chunk_length):
 
@@ -17,82 +52,89 @@ def apply_features(source, outdir, outfile, chunk_length):
 
     preprocessed = glob.glob(os.path.join(source, "*"))
 
-    total_files = len(preprocessed)
-
-    frame = pd.DataFrame(columns=[
-        'file',
-        'activity',
-        'mean_packet_size',
-        'mean_inter_packet_delay',
-        'send_receive_ratio',
-        # 'std_packet_size',
-        # 'min_packet_size',
-        # 'max_packet_size',
-        # 'std_inter_packet_delay',
-        # 'min_inter_packet_delay',
-        # 'max_inter_packet_delay',
-        'count_peaks',
-        'mean_inter_peak_delay',
-        'std_inter_peak_delay'
-    ], index=range(total_files))
-
-    for i, file in enumerate(preprocessed):
-
-        print(f"Feature engineering {file}")
-
-        # All preprocessed files follow the same naming convention -- either
-        # streaming-<originalfilename>.csv or browsing-<original>.csv
-        fname = os.path.basename(file)
-        splitted = fname.split('-')
-        activity = splitted[0]
-        originalfilename = '-'.join(splitted[1:])
-
-        # Chunk into bins, make a set of features for each bin. This ensures
-        # equal length.
-        #
-        #! TODO
-        # df['ptime'] = pd.to_timedelta(df.ptime)
-        # df = df.set_index('ptime')
-
-        # binned = df.resample('160s')
-
-        df = pd.read_csv(file)
-
-        mean_packet_size = src.features.computing.mean_packet_size(df)
-        mean_inter_packet_delay = src.features.computing.mean_inter_packet_delay(df)
-        send_receive_ratio = src.features.computing.send_receive_ratio(df)
-
-        # Resample-related features
-        df['ptime'] = pd.to_timedelta(df.ptime)
-        df = df.set_index('ptime')
-
-        bytes_per_second = df.resample('1s').psize.sum()
-
-        def threshold(ser):
-            return ser.mean() + ser.min()
-
-        peaks, props = scipy.signal.find_peaks(
-            bytes_per_second, threshold(bytes_per_second)
-        )
-
-        count_peaks = len(peaks)
-        peak_delays = df.index[peaks].total_seconds().to_series().diff()
-        mean_inter_peak_delay = peak_delays.mean()
-        std_inter_peak_delay = peak_delays.std()
+    with concurrent.futures.ProcessPoolExecutor(5) as executor:
+        signals = executor.map(get_signal, preprocessed)
+    
+    frame = pd.DataFrame(np.vstack(signals)).dropna()
+    frame.columns = ['file', 'activity', 'signal']
 
 
-        row = pd.Series({
-            'file': originalfilename,
-            'activity': activity,
-            'mean_packet_size': mean_packet_size,
-            'mean_inter_packet_delay': mean_inter_packet_delay,
-            'send_receive_ratio': send_receive_ratio,
-            'count_peaks': count_peaks,
-            'mean_inter_peak_delay': mean_inter_peak_delay,
-            'std_inter_peak_delay': std_inter_peak_delay
-        })
+    # total_files = len(preprocessed)
 
-        frame.iloc[i] = row
+    # frame = pd.DataFrame(columns=[
+    #     'file',
+    #     'activity',
+    #     'mean_packet_size',
+    #     'mean_inter_packet_delay',
+    #     'send_receive_ratio',
+    #     # 'std_packet_size',
+    #     # 'min_packet_size',
+    #     # 'max_packet_size',
+    #     # 'std_inter_packet_delay',
+    #     # 'min_inter_packet_delay',
+    #     # 'max_inter_packet_delay',
+    #     'count_peaks',
+    #     'mean_inter_peak_delay',
+    #     'std_inter_peak_delay'
+    # ], index=range(total_files))
+
+    # for i, file in enumerate(preprocessed):
+
+    #     print(f"Feature engineering {file}")
+
+    #     # All preprocessed files follow the same naming convention -- either
+    #     # streaming-<originalfilename>.csv or browsing-<original>.csv
+    #     fname = os.path.basename(file)
+    #     splitted = fname.split('-')
+    #     activity = splitted[0]
+    #     originalfilename = '-'.join(splitted[1:])
+
+    #     # Chunk into bins, make a set of features for each bin. This ensures
+    #     # equal length.
+    #     #
+    #     #! TODO
+    #     # df['ptime'] = pd.to_timedelta(df.ptime)
+    #     # df = df.set_index('ptime')
+
+    #     # binned = df.resample('160s')
+
+    #     df = pd.read_csv(file)
+
+    #     mean_packet_size = src.features.computing.mean_packet_size(df)
+    #     mean_inter_packet_delay = src.features.computing.mean_inter_packet_delay(df)
+    #     send_receive_ratio = src.features.computing.send_receive_ratio(df)
+
+    #     # Resample-related features
+    #     df['ptime'] = pd.to_timedelta(df.ptime)
+    #     df = df.set_index('ptime')
+
+    #     bytes_per_second = df.resample('1s').psize.sum()
+
+    #     def threshold(ser):
+    #         return ser.mean() + ser.min()
+
+    #     peaks, props = scipy.signal.find_peaks(
+    #         bytes_per_second, threshold(bytes_per_second)
+    #     )
+
+    #     count_peaks = len(peaks)
+    #     peak_delays = df.index[peaks].total_seconds().to_series().diff()
+    #     mean_inter_peak_delay = peak_delays.mean()
+    #     std_inter_peak_delay = peak_delays.std()
+
+
+    #     row = pd.Series({
+    #         'file': originalfilename,
+    #         'activity': activity,
+    #         'mean_packet_size': mean_packet_size,
+    #         'mean_inter_packet_delay': mean_inter_packet_delay,
+    #         'send_receive_ratio': send_receive_ratio,
+    #         'count_peaks': count_peaks,
+    #         'mean_inter_peak_delay': mean_inter_peak_delay,
+    #         'std_inter_peak_delay': std_inter_peak_delay
+    #     })
+
+    #     frame.iloc[i] = row
 
     # Make sure outdir exists
     os.makedirs(outdir, exist_ok=True)
